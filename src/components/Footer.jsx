10591ede8e01fb6e786bf42043647bab60fe0c90
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-18 16:25:54 -0300

    context y filters.js creados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import "./Footer.css";
 
-export default function Footer() {
+export default function Footer({ filters }) {
   return (
     <footer className="footer">
+      {JSON.stringify(filters, null, 2)}
+      <span>Categoría: {filters.category}</span>
+      <span>Precio: ${filters.minPrice}</span>
       <h4>
-        Prueba técnica de React ⚛️ - <span>emmanuel</span>
+        Prueba técnica de React ⚛️ － <span>emmanuel</span>
       </h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
