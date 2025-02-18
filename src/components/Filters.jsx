f6943b541741e61c2d7fd80fe00e03be2934ad72
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-18 10:55:31 -0300

    manejando filtrado en Filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import "./Filters.css";
 
-export function Filters() {
+export function Filters({ onChange }) {
   const [minPrice, setMinPrice] = useState(0);
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value);
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    onChange((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
   };
 
   return (
     <section className="filters">
       <div>
-        <label htmlFor="price">Precio</label>
+        <label htmlFor="price">Precio a partir de:</label>
         <input
           type="range"
           id="price"
@@ -24,7 +35,7 @@ export function Filters() {
 
       <div>
         <label htmlFor="category">Categoría</label>
-        <select id="category">
+        <select id="category" onChange={handleChangeCategory}>
           <option value="all">Todas</option>
           <option value="laptops">Portátiles</option>
           <option value="smartphones">Celulares</option>
