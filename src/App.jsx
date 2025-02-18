a9e61eba67f833b8dcbb304edc4abf0816242702
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-18 11:34:30 -0300

    filtros a useFilters hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { Products } from "./components/Products";
 import { useState } from "react";
 import { Header } from "./components/Header";
 
-function App() {
-  const [products] = useState(initialProducts);
+function useFilters() {
   const [filters, setFilters] = useState({
     category: "all",
     minPrice: 0,
@@ -18,7 +17,12 @@ function App() {
       );
     });
   };
+  return { filterProducts, setFilters };
+}
 
+function App() {
+  const [products] = useState(initialProducts);
+  const { filterProducts, setFilters } = useFilters();
   const filteredProducts = filterProducts(products);
 
   return (
