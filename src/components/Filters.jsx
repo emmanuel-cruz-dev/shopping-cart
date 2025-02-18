201e1be4ea765d2cf02b23c2c3d85073aec5c3e8
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-18 17:08:24 -0300

    minPrice pasado a Filters para manejar esa unica fuente de la verdad

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,15 +1,14 @@
-import { useState, useId } from "react";
+import { useId } from "react";
 import "./Filters.css";
 import { useFilters } from "../hooks/useFilters";
 
 export function Filters() {
-  const { setFilters } = useFilters();
-  const [minPrice, setMinPrice] = useState(0);
+  const { filters, setFilters } = useFilters();
+
   const minPriceFilterId = useId();
   const categoryFilterId = useId();
 
   const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value);
     setFilters((prevState) => ({
       ...prevState,
       minPrice: event.target.value,
@@ -32,9 +31,10 @@ export function Filters() {
           id={minPriceFilterId}
           min={0}
           max={1000}
+          value={filters.minPrice}
           onChange={handleChangeMinPrice}
         />
-        <span>${minPrice}</span>
+        <span>${filters.minPrice}</span>
       </div>
 
       <div>
