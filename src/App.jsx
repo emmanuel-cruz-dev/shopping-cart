82aded4ec28ac79650ae61ed47da89dc215ed7f4
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-18 16:52:07 -0300

    hook useFilters extraido de App

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Header } from "./components/Header";
 import Footer from "./components/Footer";
-import { FiltersContext } from "./context/filters";
-
-function useFilters() {
-  // const [filters, setFilters] = useState({
-  //   category: "all",
-  //   minPrice: 0,
-  // });
-
-  const { filters, setFilters } = useContext(FiltersContext);
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === "all" || product.category === filters.category)
-      );
-    });
-  };
-  return { filters, filterProducts, setFilters };
-}
+import { useFilters } from "./hooks/useFilters";
 
 function App() {
   const [products] = useState(initialProducts);
