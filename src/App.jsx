dd51e8ffcff2ebd1b9cb11cbb50a8684d1f255bd
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-18 09:53:13 -0300

    filter de productos agregado a App

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,24 @@ import { useState } from "react";
 
 function App() {
   const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
   return (
     <>
       <h1>Shopping Cart 🛒</h1>
-      <Products products={products} />
+      <Products products={filterProducts(products)} />
     </>
   );
 }
