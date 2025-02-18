48d71bdb7861c6c016d7ef2257b797b2c24a83b8
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-18 09:43:45 -0300

    cambiando path de imagenes thumbnail en el mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { products } from "./mocks/products.json";
+import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
+import { useState } from "react";
 
 function App() {
+  const [products] = useState(initialProducts);
   return (
     <>
       <h1>Shopping Cart 🛒</h1>
