3aa414736ec09dc06cee7d7ae837244495835260
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 15:47:13 -0300

    footer modificado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,14 @@ export function Footer() {
   return (
     <footer className="footer">
       <h4>
-        Prueba técnica de React ⚛️ － <span>emmanuel</span>
+        Prueba técnica de React ⚛️ －{" "}
+        <a
+          href="https://emmanuel-cruz.netlify.app/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          emmanuel
+        </a>
       </h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
