8737b5258eec1d7c3df01ad162a182e7c75a1603
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 11:54:39 -0300

    Footer actualizado, filters y cart eliminados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,12 @@
-import { useCart } from "../hooks/useCart";
-import { useFilters } from "../hooks/useFilters";
 import "./Footer.css";
 
 export function Footer() {
-  const { filters } = useFilters();
-  const { cart } = useCart();
-
   return (
     <footer className="footer">
-      {/* {JSON.stringify(filters, null, 2)} */}
-      {/* {JSON.stringify(cart, null, 2)} */}
-      {/* <span>Categoría: {filters.category}</span>
-      <span>Precio: ${filters.minPrice}</span>
       <h4>
         Prueba técnica de React ⚛️ － <span>emmanuel</span>
       </h4>
-      <h5>Shopping Cart con useContext & useReducer</h5> */}
+      <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
   );
 }
