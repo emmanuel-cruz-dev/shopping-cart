1948e08736c6eb01746135927d2ef263f75d78d9
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 12:43:30 -0300

    implementando useReducer

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,11 +1,47 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 // 1. Crear contexto
 export const CartContext = createContext();
 
+const initialState = [];
+const reducer = (state, action) => {
+  const { type: actionType, payload: actionPayload } = action;
+
+  switch (actionType) {
+    case "ADD_TO_CART": {
+      const { id } = actionPayload;
+      const productInCartIndex = state.findIndex((item) => item.id === id);
+
+      if (productInCartIndex >= 0) {
+        const newState = structuredClone(state);
+        newState[productInCartIndex].quantity += 1;
+        return newState;
+      }
+
+      return [
+        ...state,
+        {
+          ...actionPayload, // product
+          quantity: 1,
+        },
+      ];
+    }
+
+    case "REMOVE_FROM_CART": {
+      const { id } = actionPayload;
+      return state.filter((item) => item.id != id);
+    }
+
+    case "CLEAR_CART": {
+      return initialState;
+    }
+  }
+  return state;
+};
+
 // 2. Crear provider
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const addToCart = (product) => {
     // Checkear si el producto ya está en el carrito
