21dca12eb96755574a8c0d92c158c25844125ffc
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 10:57:09 -0300

    renderizado de boton agregar carrito condicional

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,20 @@
 import "./Products.css";
-import { AddToCartIcon } from "./Icons";
+import { AddToCartIcon, RemoveFromCartIcon } from "./Icons";
 import { useCart } from "../hooks/useCart";
 
 export function Products({ products }) {
-  const { addToCart } = useCart();
+  const { addToCart, cart } = useCart();
+
+  const checkProductInCart = (product) => {
+    return cart.some((item) => item.id === product.id);
+  };
 
   return (
     <main className="products">
       <ul>
         {products.slice(0, 10).map((product) => {
+          const isProductInCart = checkProductInCart(product);
+
           return (
             <li key={product.id}>
               <img src={product.thumbnail} alt={product.title} />
@@ -18,7 +24,7 @@ export function Products({ products }) {
               </div>
               <div>
                 <button onClick={() => addToCart(product)}>
-                  <AddToCartIcon />
+                  {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
                 </button>
               </div>
             </li>
