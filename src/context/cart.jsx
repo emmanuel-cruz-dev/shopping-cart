61e3819389f9e8556760409ed424c85b3197d7b9
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 12:51:10 -0300

    useReducer funcional, funciones actualizadas para usar el reducer

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -43,38 +43,27 @@ const reducer = (state, action) => {
 export function CartProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const addToCart = (product) => {
-    // Checkear si el producto ya está en el carrito
-    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
+  const addToCart = (product) =>
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: product,
+    });
 
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart);
-      newCart[productInCartIndex].quantity += 1;
-      return setCart(newCart);
-    }
+  const removeFromCart = (product) =>
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: product,
+    });
 
-    // Si el producto no está en el carrito
-    setCart((prevState) => [
-      ...prevState,
-      {
-        ...product,
-        quantity: 1,
-      },
-    ]);
-  };
-
-  const removeFromCart = (product) => {
-    setCart((prevState) => prevState.filter((item) => item.id != product.id));
-  };
-
-  const clearCart = () => {
-    setCart([]);
-  };
+  const clearCart = () =>
+    dispatch({
+      type: "CLEAR_CART",
+    });
 
   return (
     <CartContext.Provider
       value={{
-        cart,
+        cart: state,
         addToCart,
         removeFromCart,
         clearCart,
