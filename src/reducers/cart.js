cb0c5e92529a0947e08d842da161320fcdfa6691
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 15:24:03 -0300

    usando localStorage para guardar el contenido de carrito de compras

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,9 +1,15 @@
-export const cartInitialState = [];
+export const cartInitialState =
+  JSON.parse(window.localStorage.getItem("cart")) || [];
 
 export const CART_ACTION_TYPES = {
   ADD_TO_CART: "ADD_TO_CART",
   REMOVE_FROM_CART: "REMOVE_FROM_CART",
   CLEAR_CART: "CLEAR_CART",
+};
+
+// Actualizar localStorage con state para cart
+export const updateLocalStorage = (state) => {
+  window.localStorage.setItem("cart", JSON.stringify(state));
 };
 
 export const cartReducer = (state, action) => {
@@ -17,24 +23,31 @@ export const cartReducer = (state, action) => {
       if (productInCartIndex >= 0) {
         const newState = structuredClone(state);
         newState[productInCartIndex].quantity += 1;
+        updateLocalStorage(newState);
         return newState;
       }
 
-      return [
+      const newState = [
         ...state,
         {
           ...actionPayload, // product
           quantity: 1,
         },
       ];
+
+      updateLocalStorage(newState);
+      return newState;
     }
 
     case CART_ACTION_TYPES.REMOVE_FROM_CART: {
       const { id } = actionPayload;
-      return state.filter((item) => item.id != id);
+      const newState = state.filter((item) => item.id != id);
+      updateLocalStorage(newState);
+      return newState;
     }
 
     case CART_ACTION_TYPES.CLEAR_CART: {
+      updateLocalStorage(cartInitialState);
       return cartInitialState;
     }
   }
