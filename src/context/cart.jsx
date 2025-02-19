3901020ece2de275f07731fab209cf53a48f9f23
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 14:59:39 -0300

    usando reducer en cart.jsx

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,47 +1,12 @@
 import { createContext, useReducer } from "react";
+import { cartReducer, cartInitialState } from "../reducers/cart";
 
 // 1. Crear contexto
 export const CartContext = createContext();
 
-const initialState = [];
-const reducer = (state, action) => {
-  const { type: actionType, payload: actionPayload } = action;
-
-  switch (actionType) {
-    case "ADD_TO_CART": {
-      const { id } = actionPayload;
-      const productInCartIndex = state.findIndex((item) => item.id === id);
-
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state);
-        newState[productInCartIndex].quantity += 1;
-        return newState;
-      }
-
-      return [
-        ...state,
-        {
-          ...actionPayload, // product
-          quantity: 1,
-        },
-      ];
-    }
-
-    case "REMOVE_FROM_CART": {
-      const { id } = actionPayload;
-      return state.filter((item) => item.id != id);
-    }
-
-    case "CLEAR_CART": {
-      return initialState;
-    }
-  }
-  return state;
-};
-
 // 2. Crear provider
 export function CartProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState);
 
   const addToCart = (product) =>
     dispatch({
