c34e3f6028907b8bd2dd82a497d1ad9f3c3ed986
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 08:14:47 -0300

    useState borrado de App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
-import { useState } from "react";
 import { Header } from "./components/Header";
 import { useFilters } from "./hooks/useFilters";
 import Footer from "./components/Footer";
 
 function App() {
-  const [products] = useState(initialProducts);
   const { filters, filterProducts } = useFilters();
-  const filteredProducts = filterProducts(products);
+  const filteredProducts = filterProducts(initialProducts);
 
   return (
     <>
